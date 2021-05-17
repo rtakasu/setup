74a217e66b3aec80742d698a3743eceae60614b9
Rafael Takasu <rafatakasu@gmail.com>
2021-05-17 10:10:29 -0700

    Removing dev packages

--- .setup_scripts/install.sh
+++ .setup_scripts/install.sh
@@ -1,18 +1,9 @@
 # Install base packages
-apt-get install tig \
+sudo apt-get install tig \
 	neovim \
 	zsh \
 	tmux \
-	silversearcher-ag \
-	jq \
-	tree \
-	libpq-dev \ # needed for postgresql
-	node \ # Install node for use with CoC
 	curl -y
 
 # Install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install vimplug
-sh -c 'curl -fLo ~/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
